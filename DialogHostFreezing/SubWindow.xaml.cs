5575d83e46de8c5371d544b263c05d349a15c13d
Bartek Ziółkowski <bartosz.ziolkowski@edpauto.com>
2017-03-02 13:51:35 +0100

    Initial commit

---
+++ DialogHostFreezing/SubWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace DialogHostFreezing
+{
+    /// <summary>
+    /// Interaction logic for SubWindow.xaml
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogHost.Show(new TranslationDetails(), "PART_TranslationsDetailsPopup");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+    }
+}
---
+++ DialogHostFreezing/TranslationDetails.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DialogHostFreezing
+{
+    using System.Windows.Controls;
+    using MaterialDesignThemes.Wpf;
+
+    public partial class TranslationDetails : UserControl
+    {
+        public TranslationDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void DiscardButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+    }
+}
